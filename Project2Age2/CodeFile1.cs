b41d73b3a740d3d211f8a240933a0b166b7197be
twotwoga <twogarin@bk.ru>
2021-04-08 16:58:01 +0500

    Добавьте файлы проекта.

---
+++ Project2Age2/CodeFile1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+class EnteringInteger{
+    static void Main()
+    {
+        // текстовые переменые 
+        string res, txt;
+        // целочисленые переменые
+        int year = 2021, age, born;
+        // окно с полем ввода
+        res = Interaction.InputBox("В каком году вы родились?", "Год рождения");
+        // преоброзования текста в число
+        born = Int32.Parse(res);
+        // вычесление возроста
+        age = year - born;
+        txt = "тогда вам " + age + " лет";
+        //онко с сообщением
+        MessageBox.Show(txt, "Возраст");
+    }
+}
